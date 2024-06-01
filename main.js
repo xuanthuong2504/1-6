c0f0fdc8991c55040f0a85f965d1e167e9476b3c
xuanthuong2504 <nguyenxuanthuong254@gmail.com>
2024-06-01 10:41:30 +0700

    init project

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent =" Cảm ơn em đã dành thời gian để nhìn những trò trẻ con của anhh và anh có những điều muốn gởi gắm đến tình iu của anhh❤️. Cảm ơn em đã đến bên anhh những lúc anh cô đơn buồn tủi nhất.Cuộc sống thực sự chẳng có ý nghĩa gì nếu không có em. Em làm cho anh luôn cảm thấy đặc biệt và hoàn hảo. Anh chẳng biết nói gì ngoài lời cảm ơn em, anh rất yêu em💕"
+var letterContent =" Chúc bé 1/6 càng ngày càng xinh đẹp, mau lớn để còn cưới chồng ❤️. Cảm ơn bé đã đến bên anh trong thời gian qua. Vì khoảng cách khá xa nên hong tặng được cho bé món quà nào cả. Yêu nhiều lắmmmmm  💕"
 
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
